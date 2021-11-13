e9ff761582119c9f18f66c251860209842268b22
yanagiragi <qaz90473@gmail.com>
2021-11-13 13:28:29 +0800

    feat: Add some basic layout of frontend prototypes

---
+++ prototype/frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div class="block">
+      <h1 class="font-sans text-8xl text-gray-200">
+        JLPT Prototype
+      </h1>
+    </div>
+    <Main msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;  
+}
+
+body {
+  /* 
+  * Image Source: 
+  * "Webtreats Seamless Web Background in Rich Black Wood 2" by webtreats 
+  * is licensed with CC BY 2.0. To view a copy of this license, visit 
+  * https://creativecommons.org/licenses/by/2.0/  
+  */
+  background-image: url("/bg.jpg");
+}
+</style>
---
+++ prototype/frontend/src/components/Main.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="block">
+    <!-- <h1>{{ msg }}</h1> -->
+    <Card :wordProp="word" :indexProp="index" v-for="(word, index) in wordList" :key="word.text" />
+    <div class="m-8">
+      <button class="bg-blue-500 hover:bg-white text-white font-semibold hover:text-blue-700 py-2 px-4 border border-transparent hover:border-blue-500 rounded">
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './Card.vue'
+
+export default {
+  name: 'Main',
+  data: function() {
+    return {
+      "wordList":  [
+        {  
+          "rank": "N3",
+          "text": "とんでもない",
+          "example": [
+            "とんでもない要求をする"
+          ],
+          "meaning": [
+            "不合理",
+            "意想不到"
+          ],
+          "mistake": null,
+          "candidates": [
+            "とんでもない",
+            "とんでもない2",
+            "とんでもない3"
+          ]
+        },
+        {
+          "rank": "N3",
+          "text": " <rb>細</rb> <rp>( </rp><rt> ほそ </rt><rp> )</rp> <rb>長</rb> <rp>( </rp><rt> なが </rt><rp> )</rp> い   ",
+          "example": [
+            "細長い顔をしている"
+          ],
+          "meaning": [
+            "細長"
+          ],
+          "mistake": null,
+          "candidates": [
+            "とんでもない",
+            "とんでもない2",
+            "とんでもない3"
+          ]
+        }
+      ]
+    }
+  },
+  components: {
+    Card
+  },
+  props: {
+    msg: String
+  },
+  mounted: function() {
+    this.hi()
+  },
+  methods: {
+    hi: function() {
+      console.log('hi')
+      return 'hi'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
